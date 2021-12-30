2a95c7ef9ccabade2ddffc8579daf554e48d3678
Wei Zheng <wzyeo1990@gmail.com>
2021-12-30 03:01:50 +0000

    remove details from fieldlist

--- schedule/serializers.py
+++ schedule/serializers.py
@@ -5,7 +5,7 @@ class ScheduleItemSeriallizer(serializers.ModelSerializer):
 
     class Meta:
         model = ScheduleItem
-        fields = ('id', 'name', 'details', 'date', 'done')
+        fields = ('id', 'name', 'date', 'done')
     
     def save(self):
         return super().save(user=self.context['request'].user)

536851897ef273cfd89c3810e4d997a53cf3077e
Wei Zheng <wzyeo1990@gmail.com>
2021-12-30 05:11:42 +0000

    date range filter

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from schedule.models import ScheduleItem
+from rangefilter.filters import DateRangeFilter
 
 class ScheduleItemAdmin(admin.ModelAdmin):
     list_display = ('user', 'name', 'date', 'done')
-    list_filter = ('user', 'date', 'done')
+    list_filter = ('user', ('date', DateRangeFilter), 'done')
 
 
 admin.site.register(ScheduleItem, ScheduleItemAdmin)
--- wzcalendar/settings.py
+++ wzcalendar/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'rangefilter',
     'user.apps.UserConfig',
     'schedule.apps.ScheduleConfig',
 ]
